0a3c624adca17f5c8516cbc056f7de5d47d74af9
panw@shishike.com <19851128>
2017-06-03 21:00:02 +0800

    补充main方法

--- src/main/java/com/pan/tdd/App.java
+++ src/main/java/com/pan/tdd/App.java
@@ -8,7 +8,12 @@ public class App {
 	public static void main(String[] args) {
 
 		System.out.println("Game Start ----->");
+		SpecialNum specialNum = new SpecialNum(3, 5, 7);
+		NumberSounder numberSounder = new NumberSounder(specialNum);
 
+		for (int i = 1; i <=100 ; i++) {
+			System.out.println(numberSounder.getNumber(i));
+		}
 
 	}
 }
