8975af174d8a4198034bc6f458f7d2b1d31e8de9
panw@shishike.com <19851128>
2017-06-03 21:11:31 +0800

    update main class name

--- src/main/java/com/pan/tdd/Game.java
+++ src/main/java/com/pan/tdd/Game.java
@@ -3,17 +3,17 @@ package com.pan.tdd;
 /**
  * Hello world!
  */
-public class App {
+public class Game {
 
 	public static void main(String[] args) {
 
-		System.out.println("Game Start ----->");
-		SpecialNum specialNum = new SpecialNum(3, 5, 7);
+		System.out.println("Game Start <-----\n");
+		SpecialNum specialNum = new SpecialNum(3, 4, 5);
 		NumberSounder numberSounder = new NumberSounder(specialNum);
 
 		for (int i = 1; i <=100 ; i++) {
 			System.out.println(numberSounder.getNumber(i));
 		}
-
+		System.out.println("\nGame End ----->");
 	}
 }
